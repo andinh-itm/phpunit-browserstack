1e919ac34cfeee89feb23494c5419ae34d320da3
andinh-itm <32686615+andinh-itm@users.noreply.github.com>
2018-10-09 15:56:18 -0400

    Update single_test.php

--- tests/single_test.php
+++ tests/single_test.php
@@ -4,9 +4,9 @@ require 'vendor/autoload.php';
 class SingleTest extends BrowserStackTest {
 
     public function testGoogle() {
-        self::$driver->get("https://www.google.com/ncr");
-        $element = self::$driver->findElement(WebDriverBy::name("q"));
-        $element->sendKeys("BrowserStack");
+        self::$driver->get("https://test-it.drupal.gwu.edu");
+        $element = self::$driver->findElement(WebDriverBy::name("edit-search-keys"));
+        $element->sendKeys("Hello");
         $element->submit();
         self::$driver->wait(10, 500)->until(function($driver) {
           $elements = $driver->findElements(WebDriverBy::id("resultStats"));
