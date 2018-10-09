cc205f6357fb8930ac9e9abe8b066df07448fe75
andinh-itm <32686615+andinh-itm@users.noreply.github.com>
2018-10-09 16:02:07 -0400

    Update single_test.php

--- tests/single_test.php
+++ tests/single_test.php
@@ -5,7 +5,7 @@ class SingleTest extends BrowserStackTest {
 
     public function testGoogle() {
         self::$driver->get("https://test-it.drupal.gwu.edu");
-        $element = self::$driver->findElement(WebDriverBy::name("edit-search-keys"));
+        $element = self::$driver->findElement(WebDriverBy::name("search_keys"));
         $element->sendKeys("Hello");
         $element->submit();
         self::$driver->wait(10, 500)->until(function($driver) {
